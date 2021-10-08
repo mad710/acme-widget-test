ef3617f8cf8956d26c489a3b7408c0c7fd46c828
mad710 <40913594+mad710@users.noreply.github.com>
2021-10-08 18:11:29 +0530

    Intital implementaion of basket

--- basket.py
+++ basket.py
@@ -1,11 +1,27 @@
+from product import Product
+from helpers import DeliveryPriceHelper
+from offers import Offer
+
 
 class Basket:
     
-    def __init__(self):
-        pass
+    def __init__(
+        self, 
+        product_list: list, 
+        delivery_price_helper: DeliveryPriceHelper,
+        offer: Offer
+    ):
+        self.product_catalogue = {p.code: p for p in product_catalogue}
+        self.delivery_price_helper = delivery_price_helper
+        self.offer = offer
+        self.cart = []
+        
+    @property
+    def total_cart_amount(self):
+        return self.offer.get_discounted_price(self.cart)    
     
     def add(self, product_code: str) -> None:
-        pass
+        self.cart.append(product_code)
     
     def total(self) -> int:
-        pass
+        return self.delivery_price_helper(self.total_cart_amount)
