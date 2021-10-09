98c3adac55cbaa06e4d24c6fd58713189d58404c
mad710 <40913594+mad710@users.noreply.github.com>
2021-10-09 12:18:08 +0530

    added comment blocks for basket class

--- basket.py
+++ basket.py
@@ -4,6 +4,9 @@ from offers import Offer
 
 
 class Basket:
+    """
+    Basket class for shopping products
+    """
     
     def __init__(
         self, 
@@ -18,17 +21,33 @@ class Basket:
         
     @property
     def total_cart_amount(self):
+        """
+        Returns total cart amount including offers, but excluding delivery price
+        """
         return self.offer.get_discounted_price(self.cart)    
     
     def add(self, product_code: str) -> None:
+        """
+        Add a product to the basket
+        
+        :param product_code str: Code of the product to be added
+        """
         self.cart.append(
             self.product_catalogue[product_code]
         )
     
     def total(self) -> int:
+        """
+        Get the total amount to be paid for the items added
+        
+        :returns int: total amount to be paid including the delivery price
+        """
         return self.total_cart_amount + \
                self.delivery_price_helper.get_delivery_price(self.total_cart_amount)
     
     
     def clear(self):
+        """
+        Reset the cart to empty
+        """
         self.cart = []
